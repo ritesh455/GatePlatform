e1f671949f98c1e95d57d959afd2ada4d081d7df
ritesh455 <riteshpilavare9011@gmail.com>
2026-03-31 14:09:21 +0530

    Change in server file origins

--- backend/server.js
+++ backend/server.js
@@ -33,12 +33,12 @@ const PORT = process.env.PORT || 5000;
 // --- Socket.IO Setup for Real-Time Chat ---
 const io = new Server(server, {
   cors: {
-    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
     origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173"
-    ].filter(Boolean),
-    methods: ["GET", "POST"]
+      "http://localhost:5173",
+      "https://gate-platform-nqmo.vercel.app"
+    ],
+    methods: ["GET", "POST"],
+    credentials: true
   }
 });
 
@@ -118,14 +118,13 @@ io.on("connection", (socket) => {
 app.use(helmet());
 
 // CORS Configuration
-app.use(cors({ 
-    // origin: process.env.FRONTEND_URL || "http://localhost:3000",
-    origin: [
-  process.env.FRONTEND_URL,
-  "http://localhost:5173"
-].filter(Boolean),
-    credentials: true,
-}));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://gate-platform-nqmo.vercel.app"
+  ],
+  credentials: true
+}))
 
 // Rate limiting
 const limiter = rateLimit({
